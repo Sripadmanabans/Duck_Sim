90ed9e39760139ae79dd55a5cb38fbdd118e8282
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-08-27 11:48:32 -0500

    Added behavior

---
+++ src/behavior/FlyBehavior.java
@@ -0,0 +1,9 @@
+package behavior;
+
+/**
+ * This is the interface that takes care of the fly behavior
+ * Created by Sripadmanaban on 8/27/2015.
+ */
+public interface FlyBehavior {
+    void fly();
+}
---
+++ src/behavior/QuackBehavior.java
@@ -0,0 +1,9 @@
+package behavior;
+
+/**
+ * This is used to specify the quack behavior
+ * Created by Sripadmanaban on 8/27/2015.
+ */
+public interface QuackBehavior {
+    void quack();
+}
