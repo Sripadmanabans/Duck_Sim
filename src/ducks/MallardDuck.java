781afdf82aa32d26f9a5c6bbc8e3f6c249e43a39
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-08-27 11:31:56 -0500

    Added a duck type - Mallard

---
+++ src/ducks/MallardDuck.java
@@ -0,0 +1,15 @@
+package ducks;
+
+import basic.Duck;
+
+/**
+ * This is a type of duck.
+ * Created by Sripadmanaban on 8/27/2015.
+ */
+public class MallardDuck extends Duck {
+
+    @Override
+    protected void display() {
+        System.out.println("I am a Mallard Duck!!");
+    }
+}
