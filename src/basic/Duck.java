9fd05838130dc652f8f8f26145adef5c24bf6bbf
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-08-27 11:28:39 -0500

    Added basic duck class

---
+++ src/basic/Duck.java
@@ -0,0 +1,14 @@
+package basic;
+
+/**
+ * This is the basic structure for the ducks.
+ * Created by Sripadmanaban on 8/27/2015.
+ */
+public abstract class Duck {
+
+    protected void swim() {
+        System.out.println("I can swim");
+    }
+
+    protected abstract void display();
+}
