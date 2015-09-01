9ae16913ca64d0cf40e53fb8d9d0c7998f0a764a
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-09-01 13:14:16 -0500

    Added the ability to add behavior on the fly.

--- src/basic/Duck.java
+++ src/basic/Duck.java
@@ -34,7 +34,25 @@ public abstract class Duck {
     /**
      * This is the function that is used to perform the fly.
      */
-    public  void performFly() {
+    public void performFly() {
         flyBehavior.fly();
+    }
+
+    /**
+     * This function can be used to set the fly behavior on the fly.
+     *
+     * @param flyBehavior An object of a class that implements the FlyBehavior interface.
+     */
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    /**
+     * This function is used to change the quack behavior on the fly
+     *
+     * @param quackBehavior An object of a class that implements the QuackBehavior interface.
+     */
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
     }
 }
