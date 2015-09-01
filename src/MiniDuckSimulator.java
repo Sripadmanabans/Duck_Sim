e3508bfcd218a452605363947bfaf68e41db0067
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-09-01 13:07:44 -0500

    Made the duck quack and fly

--- src/MiniDuckSimulator.java
+++ src/MiniDuckSimulator.java
@@ -10,6 +10,8 @@ public class MiniDuckSimulator {
     public static void main(String args[]) {
         Duck mallard = new MallardDuck();
         mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
     }
 
 }
